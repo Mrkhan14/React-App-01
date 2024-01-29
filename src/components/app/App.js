2976bf41950fcd31956cf86cd8d28b55fc2d4b3a
Mrkhan14 <aabdulaxadkhan@gmail.com>
2024-01-29 14:57:58 +0500

    #8. New project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import Header from '../header/Header'
-import Button from '../button/Button'
-import Field from '../field/Field'
+import AppFilter from '../app-filter/app-filter'
+import AppInfo from '../app-info/app-info'
+import MovieList from '../movie-list/movie-list'
+import MoviesAddForm from '../movies-add-form/movies-add-form'
+import SearchPanel from '../search-panel/search-panel'
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Field />
-        <Button />
-    </div>
-  );
+	return (
+		<div className='app font-monospace'>
+			<div className='content'>
+				<AppInfo />
+				<div className='search-panel'>
+					<SearchPanel />
+					<AppFilter />
+				</div>
+				<MovieList />
+				<MoviesAddForm />
+			</div>
+		</div>
+	)
 }
-export default App;
+export default App
