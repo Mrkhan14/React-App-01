b7a481ded80f03bc1265293c381a1465691fd4c5
Mrkhan14 <aabdulaxadkhan@gmail.com>
2024-02-06 12:07:57 +0500

    #19. React fragment

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,14 @@
+import { Fragment } from "react";
 import './app-info.css'
 
 const AppInfo = ({allMoviesCount, favouriteMoviesCount}) => {
 	return (
-		<div className='app-info'>
-			<p className='fs-3 text-uppercase'>Barcha kinolar soni: {allMoviesCount} </p>
-			<p className='fs-4 text-uppercase'>Sevimlik filim: {favouriteMoviesCount}</p>
-		</div>
+		<Fragment>
+			<div className='app-info'>
+				<p className='fs-3 text-uppercase'>Barcha kinolar soni: {allMoviesCount} </p>
+				<p className='fs-4 text-uppercase'>Sevimlik filim: {favouriteMoviesCount}</p>
+			</div>
+		</Fragment>
 	)
 }
 
